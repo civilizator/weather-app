d53ea730a6570028dd62753403e9953c6fbfb816
StanKov <civilizator@inbox.ru>
2019-05-05 22:33:25 +0800

    components

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ import React from "react";
 class Form extends React.Component {
     render() {
         return (
-            <div>Form Component 1</div>
+            <div>Form Component</div>
         );
     }
 }
