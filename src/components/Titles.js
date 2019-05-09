04c482f414962195bf47d3741e8c2f35ebf666df
Stan Kovrigin <stan@argaatas.com>
2019-05-10 01:25:27 +0800

    2pc

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -2,11 +2,11 @@ import React from "react";
 
 class Titles extends React.Component {
     render() {
-        return(
-            <div>
+        return (
+            <>
                 <h1>Weather Finder</h1>
                 <p>Find out temperature, conditions and more...</p>
-            </div>
+            </>
         );
     }
 }
