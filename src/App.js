1a472a312f6fa9f4a1fb804db324bc6f44c6e34b
Stan Kovrigin <stan@argaatas.com>
2019-05-11 23:03:58 +0800

    2pc

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from 'react';
 
 import Titles from "./components/Titles";
 import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "6ad702c17f8ab90914ba6852623d7a86";
 
 class App extends React.Component {
+
+    getWeather = async (e) => {
+        e.preventDefault();
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Sayansk,ru&appid=${API_KEY}&units=metric`);
+        const data = await api_call.json();
+        console.log(data);
+    };
+
     render() {
         return (
             <>
                 <Titles/>
-                <Form/>
+                <Form getWeather={this.getWeather}/>
+                <Weather/>
             </>
         );
     }
