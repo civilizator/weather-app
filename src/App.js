067c781ed2814bcaf1ff2c40eaf6675790725525
StanKov <civilizator@inbox.ru>
2019-05-15 01:32:44 +0800

    components

--- src/App.js
+++ src/App.js
@@ -23,11 +23,13 @@ class App extends React.Component {
         const country = e.target.elements.country.value;
         const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
         const data = await api_call.json();
+
         console.log(data);
+
         this.setState({
-            county: data.sys.country,
-            temperature: data.main.temp,
+            country: data.sys.country,
             city: data.name,
+            temperature: data.main.temp,
             description: data.weather[0].description,
             humidity: data.main.humidity,
             error: ""
@@ -39,7 +41,14 @@ class App extends React.Component {
             <>
                 <Titles/>
                 <Form getWeather={this.getWeather}/>
-                <Weather/>
+                <Weather
+                    country={this.state.country}
+                    city={this.state.city}
+                    temperature={this.state.temperature}
+                    description={this.state.description}
+                    humidity={this.state.humidity}
+                    error={this.state.error}
+                />
             </>
         );
     }
