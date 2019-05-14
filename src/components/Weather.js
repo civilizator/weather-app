ac9b4e8a882e4703b7e3fcf0925bf969682646f3
StanKov <civilizator@inbox.ru>
2019-05-15 01:59:04 +0800

    Upd

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,7 +6,7 @@ class Weather extends React.Component {
         return (
             <div>
                 {this.props.country && this.props.city && <p>Location: {this.props.country}, {this.props.city}</p>}
-                <p>Temperature: {this.props.temperature}</p>
+                {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
                 <p>Conditions: {this.props.description}</p>
                 <p>Humidity: {this.props.humidity}</p>
                 <p>{this.props.error}</p>
